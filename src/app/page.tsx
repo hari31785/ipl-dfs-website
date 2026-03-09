b457aefc84734534074465d0d4d85e4e28fa41ce
Hari Kurada <harikurada@Haris-MacBook-Pro.local>
2026-03-09 15:58:27 -0400

    Remove admin portal link from hero section, keep only in footer under Support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,19 +63,6 @@ function HomeContent() {
                 Sign In
               </Link>
             </div>
-            
-            {/* Admin Portal Link */}
-            <div className="mt-8 pt-4 border-t border-white/20">
-              <Link 
-                href="/admin/login" 
-                className="inline-flex items-center text-blue-200 hover:text-white transition-colors text-sm"
-              >
-                <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd" />
-                </svg>
-                Administrator Portal
-              </Link>
-            </div>
           </div>
         </div>
       </div>
